78d9c5d7994929023704ccb6cbcd07e1e6e49a10
james dunnam <james@dunnam.dev>
2020-10-07 16:10:50 -0400

    cleanup(misc): cleanup formatter

--- scripts/check-format.sh
+++ scripts/check-format.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
 
-echo "Checking Prettier"
+printf "Checking Prettier... 🔎\n"
 prettier "**/*.{ts,js,json,css,scss,md,yml,graphql}" --list-different --ignore-path=.prettierignore
 PRETTIER_STATUS=$?
 
-echo "Checking ESLint"
+printf "\nChecking ESLint... 🔎"
 eslint '**/*.{js,ts}' --ignore-path=.eslintignore
 ESLINT_STATUS=$?
 
-echo "Checking CRLF"
-find scripts -type f -exec file "{}" ";" | grep CRLF
+printf "\nChecking CRLF... 🔎"
+find . -type f \
+  ! -path "*/node_modules/*" \
+  ! -path "*/coverage/*" \
+  ! -path "*/build/*" \
+  ! -path "*/.*/*" \
+  -exec file "{}" ";" | grep CRLF
 SCRIPTS_STATUS=$?
 
-echo "    ...Checking apps 🔎"
-find apps -type f ! -path "*/node_modules/*" -exec file "{}" ";" | grep CRLF
-APPS_STATUS=$?
+printf "\n"
 
-echo "    ...Checking libs 🔎"
-find libs -type f ! -path "*/node_modules/*" -exec file "{}" ";" | grep CRLF
-LIBS_STATUS=$?
-
-if [[ $PRETTIER_STATUS -eq 1 || $ESLINT_STATUS -eq 1 || $SCRIPTS_STATUS -eq 0 || $APPS_STATUS -eq 0 || $LIBS_STATUS -eq 0 ]]; then
+if [[ $PRETTIER_STATUS -eq 1 || $ESLINT_STATUS -eq 1 || $SCRIPTS_STATUS -eq 0 ]]; then
   echo "Please run 'yarn format'"
   exit 1
 fi
