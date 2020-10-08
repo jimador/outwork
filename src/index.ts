b5379df028afc69d565dbfabba314c73a93b3795
james dunnam <james@dunnam.dev>
2020-10-08 09:08:31 -0400

    cleanup(repo): cleanup exports and tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-export * from './retry'
-export * from './backoff'
-export * from './error'
+import {
+  BackOff,
+  BackOffPolicy,
+  ConstantBackOffConfiguration,
+  ExponentialBackoffConfiguration,
+  RetryConfiguration,
+} from './backoff'
+import { RetryError } from './error'
+import { retry, withRetry } from './retry'
+
+export {
+  retry,
+  withRetry,
+  BackOffPolicy,
+  BackOff,
+  RetryError,
+  ExponentialBackoffConfiguration,
+  RetryConfiguration,
+  ConstantBackOffConfiguration,
+}
