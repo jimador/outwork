2c31dfbe296154c28343945455b38335481b4758
james dunnam <james@dunnam.dev>
2021-08-08 20:23:24 -0400

    fix(repo): fix type error

--- src/utils.ts
+++ src/utils.ts
@@ -2,5 +2,5 @@
  * Waiting function
  * @param sleepTimeMs - time to wait in milliseconds.
  */
-export const wait = async (sleepTimeMs: number): Promise<number> =>
+export const wait = async (sleepTimeMs: number): Promise<void> =>
   new Promise((resolve) => setTimeout(resolve, sleepTimeMs))
